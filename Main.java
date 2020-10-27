8712e8fdeeb9843a4c74d189d9b9f6f61653f93f
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-10-27 12:25:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class main
+{
+   public static void main(String[] args)
+   {
+       // Array example
+       int[] prices = {10};
+       // Add an array of 5 doubles called prices.
+           double[] prices = new double[5];
+       // Add an array of 5 Strings called names.
+       String[] names = {"Jamal", "Emily", "Destiny", "Mateo", "Sofia"};
+       
+       System.out.println("Array highScores declared with size " + prices.length);
+       // Print out the length of the new arrays
+       
+   }
+}
